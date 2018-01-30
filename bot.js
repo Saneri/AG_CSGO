7aa8524d81ecf557e04b343348be03d159c7d1f7
Santeri Suitiala <santeri.suitiala@aalto.fi>
2018-01-30 22:09:52 +0200

    Implement some of the default commands

--- bot.js
+++ bot.js
@@ -7,6 +7,8 @@ const trueskill = require("trueskill");
 const bot = new discord.Client();
 
 const maxQueueSize = 10;
+const defaultRating = 1000;
+var matchRunning = false;
 var queue = [];
 
 const logger = winston.createLogger({
@@ -50,15 +52,14 @@ bot.on("message", async message => {
 		case "joinqueue":
 			var sql = `SELECT name FROM players WHERE name='${message.author}'`;
 			con.query(sql, function(err, result) {
-				console.log(result);
 				if (err) {
 					logger.error(err);
 					message.channel.send("Database error. Contact admin");
 				} else {
 					// Add player to database if not found
-					if (!sql) {
-						var addplayer = `INSERT INTO players (name, ign, rating, decaying) VALUES ('${message.author}', '${args.shift()}', 500, 0)`;
-						con.query(addplayer, function(err, result) {
+					if (result.length === 0) {
+						var addplayer = `INSERT INTO players (name,ign,rating,decaying) VALUES ('${message.author}', '${args.shift()}',${defaultRating} , 0)`;
+						con.query(addplayer, function(err) { //,result
 							if (err) {
 								message.channel.send(`Adding ${message.author} to database failed.`);
 								logger.error(err);
@@ -75,54 +76,114 @@ bot.on("message", async message => {
 					} else {
 						message.channel.send(`${message.author} is already in the queue.`);
 					}
+					if (queue.length === maxQueueSize) startMatch();
 				}  
 			});
 			break;
 		
 		
-		// !leavequeue
-		case "leavequeue":
-			if (queue.includes(message.author)) {
-				queue.splice(queue.indexOf(message.author), 1);
-				message.channel.send(`${message.author} has left the queue.`);
-			} else {
-				message.channel.send(`${message.author} is not in the queue.`);
-			}
-				
-			break;
-		
-		// !queue	
-		// Displays queue size and its members.
-		case "queue":
-			if (queue.length == 0) {
-				message.channel.send("Queue is empty.");
-			} else {
-				var queuestatus = "";
-				queuestatus += `Queue: ${queue.length}/${maxQueueSize}`;
-				for (var i=0, len=queue.length; i<len; i++) {
-					queuestatus += `\n ${queue[i].username}`;
+			// !leavequeue
+			case "leavequeue":
+				if (queue.includes(message.author)) {
+					queue.splice(queue.indexOf(message.author), 1);
+					message.channel.send(`${message.author} has left the queue.`);
+				} else {
+					message.channel.send(`${message.author} is not in the queue.`);
 				}
-				
-				message.channel.send(queuestatus);
-			}
-			break;
-		
-		// !rating
-		// Displays the rating and ladder position of the message author
-		case "rating":
-			break;
-		
-		// !ladder
-		// Displays the whole ladder with positions and player names
-		case "ladder":
-			break;
-		
-		// !commands
-		// Info for new users. Descripes the commands
-		case "commands":
-			break;
+					
+				break;
+			
+			// !queue	
+			// Displays queue size and its members.
+			case "queue":
+				if (queue.length == 0) {
+					message.channel.send("Queue is empty.");
+				} else {
+					var queuestatus = "";
+					queuestatus += `Queue: ${queue.length}/${maxQueueSize}`;
+					for (var i=0, len=queue.length; i<len; i++) {
+						queuestatus += `\n ${queue[i].username}`;
+					}
+					if (message.channel.type === "text") {
+						message.channel.send(queuestatus);
+					} else {
+						message.author.send(queuestatus);
+					}
+				}
+				break;
+				// !rating
+				// Displays the rating and ladder position of the message author
+			case "rating":
+				var sql = `SELECT name FROM players WHERE name='${message.author}'`;
+				con.query(sql, function(err, result) {
+					var rating = "";
+					if (err) {
+						logger.error(err);
+						message.channel.send("Database error. Contact admin");
+						return;
+					} else if (result.length != 0) {
+						rating = `${message.author}'s rating is: ${result[0].rating}`;
+					} else {
+						rating = `${message.author} doesn't have a rating yet.`;
+					}
+					if (message.channel.type === "text") {
+						message.channel.send(rating);
+					} else {
+						message.author.send(rating);
+					}
+				});
+				break;
+			
+			// !ladder
+			// Displays the whole ladder with positions and player names
+			case "ladder":
+				var sql = "SELECT name, rating FROM players";
+				con.query(sql, function(err, result) {
+					if (err) {
+						logger.error(err);
+						message.channel.send("Database error. Contact admin");
+					} else {
+						
+					}
+				});
+				break;
+			
+			// !commands and !help
+			// Info for new users. Descripes the commands
+			case "commands":
+			case "help":
+				message.author.send({embed: {
+				    color: 3447003,
+				    title: "--- Commands ---",
+				    fields: [{
+				        name: "!joinqueue",
+				        value: "Join the queue if it's not currently full. Adds new players to ladder."
+				    },
+				    {
+				    	name: "!leavequeue",
+				    	value: "Leave the queue."
+				    },
+				    {
+				    	name: "!queue",
+				    	value: "Display current queue and it's members."
+				    },
+				    {
+				    	name: "!rating",
+				    	value: "Display your current rating and position in the ladder."
+				    }],
+				  }
+				});
+						
+				break;
+			
 	}
 });
+
+
+function startMatch() {
+	//start match here, lock queue, give players connect info
+	logger.info("/n --- Match begin ---")
+}
 
 // Start bot
 bot.login(auth.token);
